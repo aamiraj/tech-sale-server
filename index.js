00d033b552bafe02caaab704425d5478f4cdb4e7
Abdullah Miraj <miraj2465@gmail.com>
2022-11-27 21:11:29 +0600

    load laptops bu user email api added

--- index.js
+++ index.js
@@ -15,23 +15,90 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function getCategory(brand){
+  switch (brand) {
+    case "HP":
+      return "1";
+      break;
+    case "Dell":
+      return "2";
+      break;
+    case "Asus":
+      return "3";
+      break;
+    case "Acer":
+      return "4";
+      break;
+    case "Lenovo":
+      return "5";
+      break;
+    case "Apple":
+      return "6";
+      break;
+    default:
+      return "0";
+  }
+};
+
+//console.log(getCategory("Apple"))
+
 const dbName = "tslaptops";
 
 async function main() {
   try {
     const db = client.db(dbName);
     const categoriesCollection = db.collection("categories");
-    const laptopsCollection = db.collection("laptops");
+    const laptopsCollection = db.collection("sellingLaptops");
     const usersCollection = db.collection("users");
 
-    app.get('/users', async (req, res)=>{
-      const email = req.query.email
-      const query = {email:email}
-      
-      const result = await usersCollection.findOne(query)
-      
-      res.send(result)
+    app.get("/products", async(req, res)=>{
+      const query = {email: req.query.email}
+      const laptopsByUser = await laptopsCollection.find(query).toArray();
+      res.send(laptopsByUser)
     })
+
+    app.post("/products", async (req, res) => {
+      const {
+        seller,
+        email,
+        product,
+        img,
+        brand,
+        location,
+        resalePrice,
+        originalPrice,
+        yearsOfUse,
+        postedTime,
+        verifiedUser
+      } = req.body;
+
+      const doc = {
+        product: product,
+        seller: seller,
+        img: img,
+        location: location,
+        resalePrice: parseFloat(resalePrice),
+        originalPrice: parseFloat(originalPrice),
+        yearsOfUse: parseInt(yearsOfUse),
+        postedTime: postedTime,
+        verifiedUser: verifiedUser,
+        categoryId: getCategory(brand),
+        brand: brand,
+        email: email,
+      };
+      console.log(doc);
+      const result = await laptopsCollection.insertOne(doc);
+      res.send(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+
+      const result = await usersCollection.findOne(query);
+
+      res.send(result);
+    });
 
     app.post("/users", async (req, res) => {
       const { name, email, role } = req.body;
@@ -40,12 +107,12 @@ async function main() {
         name: name,
         email: email,
         role: role,
-        verified: false
+        verified: false,
       };
 
-    const result = await usersCollection.insertOne(doc)
-      
-    res.send(result);
+      const result = await usersCollection.insertOne(doc);
+
+      res.send(result);
     });
 
     app.get("/categories/:id", async (req, res) => {
